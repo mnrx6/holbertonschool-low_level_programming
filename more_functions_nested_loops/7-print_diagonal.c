7b44709420108f56e9fc05a2b588732d2b8f4fb7
mnrx6 <mnrx78@gmail.com>
2026-07-14 10:52:27 +0000

    Add print_diagonal function

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a diagonal line
+ * @n: Number of times '\' should be printed
+ *
+ * Return: Nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
