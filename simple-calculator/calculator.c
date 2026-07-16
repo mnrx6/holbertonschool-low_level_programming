4cfadd36511eb2de01236c45b275bdcde14f2ce0
mnrx6 <mnrx78@gmail.com>
2026-07-16 18:26:02 +0000

    Add task 1

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -6,21 +6,28 @@
  */
 int main(void)
 {
-	int choice;
+	int choice = 1;
 
-	printf("Simple Calculator\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
-	printf("Choice: ");
-
-	scanf("%d", &choice);
-
-	if (choice == 0)
+	while (choice != 0)
 	{
-		printf("Bye!\n");
+		printf("Simple Calculator\n");
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
+
+		scanf("%d", &choice);
+
+		if (choice < 0 || choice > 4)
+		{
+			printf("Invalid choice\n");
+		}
+		else if (choice == 0)
+		{
+			printf("Bye!\n");
+		}
 	}
 
 	return (0);
