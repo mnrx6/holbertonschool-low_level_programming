88d1c8d373f12fdc0f2d9add60a53c561ca3c405
mnrx6 <mnrx78@gmail.com>
2026-07-17 17:15:07 +0000

    Complete task 5: rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char temp;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	j = 0;
+
+	while (j < i)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+
+		j++;
+		i--;
+	}
+}
