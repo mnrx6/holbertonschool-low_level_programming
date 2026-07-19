75d8a9f1774e8add25fb6fe4e63a4fd5939fb671
mnrx6 <mnrx78@gmail.com>
2026-07-19 08:50:53 +0000

    Complete task 10: atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: converted integer
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	unsigned int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				num = (num * 10) + (s[i] - '0');
+				i++;
+			}
+			return (num * sign);
+		}
+		i++;
+	}
+
+	return (0);
+}
