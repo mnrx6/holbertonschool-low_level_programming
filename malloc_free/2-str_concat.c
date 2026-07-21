ce6f012cbb2e8bfc39465b8f6eff35d43b4b442d
mnrx6 <mnrx78@gmail.com>
2026-07-21 10:49:03 +0000

    Fix str_concat Betty style

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,53 +5,35 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: pointer to newly allocated concatenated string,
- * or NULL on failure
+ * Return: pointer to new string, or NULL
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i;
-	unsigned int j;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	len1 = 0;
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
-	len2 = 0;
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	str = malloc((len1 + len2 + 1) * sizeof(char));
 	if (str == NULL)
-	{
 		return (NULL);
-	}
 
-	i = 0;
 	while (i < len1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
 
-	j = 0;
 	while (j < len2)
 	{
 		str[i] = s2[j];
