7e720792eea194785d16a99e7cfdd3c5a0a6f2c8
mnrx6 <mnrx78@gmail.com>
2026-07-25 09:06:55 +0000

    Fix factorial function

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,17 +5,13 @@
  *
  * Return: factorial of n, or -1 if n is negative
  */
-c
+int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
 
 	if (n == 0)
-	{
 		return (1);
-	}
 
 	return (n * factorial(n - 1));
 }
