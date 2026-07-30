3a01a2548470d976afad6898ca2b12958a7615f1
mnrx6 <mnrx78@gmail.com>
2026-07-30 11:11:03 +0000

    fix taske 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+
 /**
  * init_dog - initializes a variable of type struct dog
  * @d: pointer to the dog structure
@@ -8,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
