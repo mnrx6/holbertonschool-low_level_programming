b9983ab618b6909ec1491f888f7c94563ff0d408
mnrx6 <mnrx78@gmail.com>
2026-07-31 12:09:27 +0000

    add taske 5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees a dog
+ * @d: pointer to the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
