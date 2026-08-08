7c180bc93e23512098fd9bbb541db120f058cec9
mnrx6 <mnrx78@gmail.com>
2026-08-08 20:05:11 +0000

    add all files

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to the head of the list
+ * @str: string to add
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a list
+ * @head: pointer to the head of the list
+ * @str: string to add
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
+	return (new);
+}
