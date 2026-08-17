3a86d84f87ee516365e0daa3e258741212d475a5
mnrx6 <mnrx78@gmail.com>
2026-08-17 10:40:59 +0000

    fix taske 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -16,19 +16,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
 
 	buf = malloc(letters);
-
 	if (buf == NULL)
-	{
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 	{
 		free(buf);
@@ -36,7 +30,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_read = read(fd, buf, letters);
-
 	if (bytes_read == -1)
 	{
 		close(fd);
@@ -45,16 +38,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-
-	if (bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buf);
-		return (0);
-	}
-
 	close(fd);
 	free(buf);
+
+	if (bytes_written != bytes_read)
+		return (0);
 
 	return (bytes_written);
 }
