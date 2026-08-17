f33dbbd714e2f20c32e8e7fbf6397157d7a1c8f4
mnrx6 <mnrx78@gmail.com>
2026-08-17 10:38:18 +0000

    add files and taske 0

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * read_textfile - reads a text file and prints it to standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t bytes_read, bytes_written;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buf = malloc(letters);
+
+	if (buf == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	bytes_read = read(fd, buf, letters);
+
+	if (bytes_read == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+
+	if (bytes_written != bytes_read)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	close(fd);
+	free(buf);
+
+	return (bytes_written);
+}
